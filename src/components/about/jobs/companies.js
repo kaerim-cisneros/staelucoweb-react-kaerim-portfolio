2783529f7943d2d1518b6a219ad4c4a0ae5ffe03
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-08-28 01:06:25 -0600

    Create the work history pages and add style

--- src/components/about/jobs/companies.js
+++ src/components/about/jobs/companies.js
@@ -13,12 +13,59 @@ class Companies extends Component {
             <div className="job-history-header">
                 <div className="job-history-header-link">    
                     <NavLink exact to="/freelance"> 
-                    <FontAwesomeIcon icon="arrow-right"/> Freelance
+                        Freelance <FontAwesomeIcon icon="arrow-right"/>
                     </NavLink>
                 </div>
                 <div className="job-history-header-txt">
                 Companies
                 </div>
+            </div>
+            <div className="job-history-wrp">
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-name">
+                        Vivint Solar
+                    </div>
+                    <div className="job-history-wrp-company-position">
+                        Customer Care Team Lead
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        Apr 2018 - Actual
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        Started as a customer care agent. I move to a different teams such as Outbound, Transfers, Special Opperations where I win the award for agent of the month 2 months in a row, as a Customer Care Team Lead I'm helpinf to the developmnet and grow of the agents, also help to keep the queue down and make desitions about escalated situations, I supervice around 25 agents.
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-name">
+                        Sykes
+                    </div>
+                    <div className="job-history-wrp-company-position">
+                        Customer Service Agent for Capital One
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        Jul 2017 - Apr 2018
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        I was awarded as top 4 performer of the month for the month of December 2017 also get the awarded for punctuality, I deliver a high standard customer service with the top privacy policies standards, helping customers during emergencies like hurricane Maria and Harvey.
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-name">
+                        Match Analysis Mexico
+                    </div>
+                    <div className="job-history-wrp-company-position">
+                        Soccer Analyst
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        Jan 2014 - Oct 2014
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        I analyze the statistics of soccer matches for the Liga MX, MLS (Major Professional Soccer Leagues) also some college matches for universities like BYU, Oregon State, etc.
+                    </div>
+                </div>
+
             </div>
             
         </div>
--- src/components/about/jobs/freelance.js
+++ src/components/about/jobs/freelance.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import Sateluco from "./../../../../static/assets/img/sateluco.jpg"
+
 class Freelance extends Component {
     constructor() {
       super();
@@ -13,12 +15,135 @@ class Freelance extends Component {
             <div className="job-history-header">
                 <div className="job-history-header-link">      
                     <NavLink exact to="/companies"> 
-                        <FontAwesomeIcon icon="arrow-right"/> Companies
+                        Companies <FontAwesomeIcon icon="arrow-right"/>
                     </NavLink>
                 </div>
                 <div className="job-history-header-txt">
                     Freelance
                 </div>
+            </div>
+            <div className="job-history-wrp">
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-image">
+                        <div className="job-history-wrp-company-image-wrapper">
+                            <img src={Sateluco} />
+                        </div>
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        As a freelancer I used the brand Sateluco Producciones the brand have different branches as a the Web Branch, the Graphic Design branch, the Video Branch and Photo KAE as the Photography Branch, originally the brand was created for video and events production but it evolves to cover the needs of my customers. 
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-position">
+                        Aram Ledezma Real Estate - Realtypath
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        Real Estate Agent // 2015 - 2016
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        Create graphic design for web advertising, design and create newsletters using html, real estate photograpy and video of the house.
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-position">
+                        Utah Valley Real Estate Investors Association
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        Real Estate // 2015
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        Create graphic design advertising for web usage using Adobe Flash
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-position">
+                        Veberka Bros. Productions - Passfire
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        Filmmaker // 2013
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        I was the fixer for the 13 days of shooting in Mexico on the documental Passfire, we shoot the festivities in the town of Tultepec, Mexico home of the mexican pyrotechnics. 
+
+                        <div className="job-history-wrp-company-description-link">
+                            <a href="https://www.youtube.com/watch?v=fHEEoRDWusk" target="_blank">Trailer</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-position">
+                        Lomas Verdes 5ta Seccion
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        HOA // 2012
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        Code and design of web app from scratch also did some logos and graphic design of presentation cards.
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-position">
+                        Grupo Cengall
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        Insurence Brocker // 2009 2012
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        Update a legacy web app using HTML, Adobe Flash and CSS www.estoyseguromx.com and www.aseguramimundo, also some graphic design content.
+
+                        <div className="job-history-wrp-company-description-link">
+                            <a href="https://www.estoyseguromx.com" target="_blank">Estoy Seguro</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-position">
+                        XEIPN Canal Once - Energia
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        TV Channel // 2008 
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        For most of the time I help the logistics for the recording of the show, also some times I served as scouter, wardrobe supervisor and was able to direct a chapter.
+                        <div className="job-history-wrp-company-description-link">
+                            <a href="https://www.youtube.com/playlist?list=PLrFkZrRQk9nkT5mH0ygJbvMi3f2T3pVLr" target="_blank">YouTube</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-position">
+                        Mantequilla Records
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        Music Production // 2009 
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        Helping the recording of the making of and review of the Goliath Festival as a cameraman.
+                        <div className="job-history-wrp-company-description-link">
+                            <a href="https://www.youtube.com/watch?v=smO1JZ2895c" target="_blank">YouTube</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="job-history-wrp-company">
+                    <div className="job-history-wrp-company-position">
+                        Riff 111
+                    </div>
+                    <div className="job-history-wrp-company-years">
+                        Web Radio // 2007 - 2008 
+                    </div>
+                    <div className="job-history-wrp-company-description">
+                        Producer and host of a radio show called "El Lenguaje del Rock", the show had the objective of play international rock.
+                    </div>
+                </div>
+
             </div>
          
         </div>
